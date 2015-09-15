c5ae54f097782f06f8b59e450d201bd6cd59cf4d
julian.wolf <julian.wolf@mail.mcgill.ca>
2015-09-15 13:48:05 -0400

    fix syntax and semantics of find_zero_deg; ignore currently-bad datum

--- find_zero_deg.m
+++ find_zero_deg.m
@@ -1,3 +1,4 @@
+function [intercept fh_intercept] = find_zero_deg
 spectrum_zeroing_130deg = get_spectrum('data/Main_zeroing_130deg.chn');
 spectrum_zeroing_135deg = get_spectrum('data/Main_zeroing_135deg.chn');
 spectrum_zeroing_140deg = get_spectrum('data/Main_zeroing_140deg.chn');
@@ -13,25 +14,25 @@ zeroing_spectra = [spectrum_zeroing_130deg spectrum_zeroing_230deg ...
                    spectrum_zeroing_145deg spectrum_zeroing_215deg];
                  
 zeroing_spectra_normalized = zeros(numel(zeroing_spectra), 1024);
-for i = 1:numel(zeroing_spectra_normalized)
-    zeroing_spectra_normalized(i) = ...
-        normalize_spectrum(zeroing_spectrums(i));
+for i = 1:numel(zeroing_spectra)
+    zeroing_spectra_normalized(i, :) = ...
+        normalize_spectrum(zeroing_spectra(i));
 end
 
-lims_50 = 400:480;
+lims50 = 400:480;
 f130 = fit(lims50.', zeroing_spectra_normalized(1, lims50).', 'gauss1');
 f230 = fit(lims50.', zeroing_spectra_normalized(2, lims50).', 'gauss1');
-lims_45 = 420:520;
+lims45 = 420:520;
 f135 = fit(lims45.', zeroing_spectra_normalized(3, lims45).', 'gauss1');
 f225 = fit(lims45.', zeroing_spectra_normalized(4, lims45).', 'gauss1');
-lims_40 = 440:560;
+lims40 = 440:560;
 f140 = fit(lims40.', zeroing_spectra_normalized(5, lims40).', 'gauss1');
 f220 = fit(lims40.', zeroing_spectra_normalized(6, lims40).', 'gauss1');
-lims_35 = 460:600;
+lims35 = 460:600;
 f145 = fit(lims35.', zeroing_spectra_normalized(7, lims35).', 'gauss1');
 f215 = fit(lims35.', zeroing_spectra_normalized(8, lims35).', 'gauss1');
 
-f_left  = polyfit([130 135 140 145].', [f130.b1 f135.b1 f140.b1 f145.b1].', 1);
+f_left  = polyfit([130 135 140].', [f130.b1 f135.b1 f140.b1].', 1);
 f_right = polyfit([215 220 225 230].', [f215.b1 f220.b1 f225.b1 f230.b1].', 1);
 
 intercept = (f_right(2)-f_left(2))/(f_left(1)-f_right(1))
@@ -43,5 +44,5 @@ plot([120 240], f_right(1) * [120 240] + f_right(2));
 plot([130 135 140 145], [f130.b1 f135.b1 f140.b1 f145.b1], 'o');
 plot([215 220 225 230], [f215.b1 f220.b1 f225.b1 f230.b1], 'o');
 hold off
- 
-clearvars -except intercept fh_intercept
+
+end
