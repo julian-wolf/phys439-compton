12964f5152f590d5e4d835b2e6595995e51ce980
julian.wolf <julian.wolf@mail.mcgill.ca>
2015-09-18 17:16:53 -0400

    update get_electron_mass to output standard deviation in stead of 95% confidence bounds

--- get_electron_mass.m
+++ get_electron_mass.m
@@ -7,6 +7,6 @@ freq_shift_slope  = freq_shift_fit.p1;
 slope_error            = (freq_shift_errors(2,1)-freq_shift_errors(1,1))/2;
 slope_error_fractional = slope_error / freq_shift_slope;
 
-mass       = 1 / freq_shift_slope;
-mass_error = slope_error_fractional * mass;
+mass       = 1 / freq_shift_slope;              % devide the given error by 2
+mass_error = slope_error_fractional * mass / 2; % since it's based on 95% lims
 end
