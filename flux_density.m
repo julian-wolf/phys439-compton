6f2da3108e8b63c70b20755bb3c13c900c8e9564
julian.wolf <julian.wolf@mail.mcgill.ca>
2015-10-03 20:35:53 -0400

    fix inefficiency computation; switch to meters; fix error in conversion to energy

--- flux_density.m
+++ flux_density.m
@@ -3,28 +3,43 @@ if nargin < 1
     draw_figure = false;
 end
 
-spectrum_4bricks = get_spectrum('data/flux_density_4bricks.chn');
-spectrum_3bricks = get_spectrum('data/flux_density_3bricks.chn');
-spectrum_2bricks = get_spectrum('data/flux_density_2bricks.chn');
-spectrum_1bricks = get_spectrum('data/flux_density_1bricks.chn');
+calib_coeffs = find_calib_coeff(false);
+
+spectrum_4bricks = ...
+    normalize_spectrum(get_spectrum('data/flux_density_4bricks.chn'));
+spectrum_3bricks = ...
+    normalize_spectrum(get_spectrum('data/flux_density_3bricks.chn'));
+spectrum_2bricks = ...
+    normalize_spectrum(get_spectrum('data/flux_density_2bricks.chn'));
+spectrum_1bricks = ...
+    normalize_spectrum(get_spectrum('data/flux_density_1bricks.chn'));
 spectrum_1brick_different = ...
-                   get_spectrum('data/flux_density_1brick_thinner.chn');
+    normalize_spectrum(get_spectrum('data/flux_density_1brick_thinner.chn'));
 
 spectra_bricks = [spectrum_1brick_different spectrum_1bricks ...
                   spectrum_2bricks spectrum_3bricks spectrum_4bricks];
               
-thicknesses = [6.7 9.3 16.0 22.7 29.4];
+thicknesses = [6.7 9.3 16.0 22.7 29.4] ./ 100;
 
 fit_range = 600:1024;
+
+detector_efficiency = @(E) (2.5e-12*E.^5 - 6.3e-9*E.^4 + 5.9e-6*E.^3 - ...
+                            2.3e-3*E.^2 + 0.17*E + 95) ./ 100;
 
 hit_counts = zeros(size(thicknesses));
 
 for i = 1:5
-    hits = double(spectra_bricks(i).data(fit_range));
+    hits = double(spectra_bricks(fit_range, i));
     f = fit(fit_range.', hits, 'gauss1');
-    
     fwhm_range = ceil(f.b1 - f.c1) : floor(f.b1 + f.c1);
-    hit_counts(i) = sum(spectra_bricks(i).data(fwhm_range));
+    
+    relevant_data  = double(spectra_bricks(fwhm_range, i));
+    corrected_data = relevant_data ./ ...
+                     detector_efficiency(calib_coeffs(relevant_data));
+    hit_counts(i)  = sum(corrected_data);
+    
+    figure;
+    plot(relevant_data, corrected_data, 'o');
 end
 
 flux_fit = fit(thicknesses.', hit_counts.', 'exp1');
@@ -34,7 +49,7 @@ if draw_figure
     figure;
     hold on
     
-    xlim([-2 30]);
+    xlim([-2 30] ./ 100);
     plot(flux_fit);
     plot(thicknesses, hit_counts, '.');
     plot(0, density, 'o');
